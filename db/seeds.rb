3282d12ef1b6b0086c9e2e72a3074f63d0e9acd3
Kate Studwell <katestudwell@gmail.com>
2016-05-16 16:46:43 -0400

    Add basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+time = DateTime.now
+num = rand * 10 - 5
+
+10.times do
+  # time = (time - 1.second/208.in_milliseconds.to_f).strftime('%N')
+  time = time - 1.minute
+  Sample.create(accel_x: num, accel_y: num, accel_z: num, sample_time: time )
+end
