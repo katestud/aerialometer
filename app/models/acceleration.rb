f5cc516b93f61d96a47826a0ff3f141d465d9868
Kate Studwell <katestudwell@gmail.com>
2016-05-17 11:51:32 -0400

    Split calculations into different methods

--- app/models/acceleration.rb
+++ app/models/acceleration.rb
@@ -4,17 +4,28 @@ class Acceleration
   end
 
   def acceleration
-    Math.sqrt(@sample.acceleration_x ** 2 + @sample.acceleration_y ** 2 + @sample.acceleration_z ** 2).round(5)
+    Math.sqrt(x_squared + y_squared + z_squared).round(5)
   end
 
+  def square(number)
+    number ** 2
+  end
+
+  def x_squared
+    square(@sample.acceleration_x)
+  end
+
+  def y_squared
+    square(@sample.acceleration_y)
+  end
+
+  def z_squared
+    square(@sample.acceleration_z)
+  end
 
 
   def performance_index
     @sample.performance_index
   end
-
-  # def time
-  #   @sample.sample_time.to_i
-  # end
 
 end
