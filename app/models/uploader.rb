de09ed6c94e04ba181e43eb1531c4fd7a56e053c
Kate Studwell <katestudwell@gmail.com>
2016-06-05 19:11:05 -0400

    Revert "Import CSV data in chunks instead of processing all at once"
    This reverts commit d7b95672428edecc46308bd505de66b88a31785f.

--- app/models/uploader.rb
+++ app/models/uploader.rb
@@ -1,38 +1,20 @@
 class Uploader
   def initialize(path, performance_id)
-    SmarterCSV.process(path, {chunk_size: 10000}) do |chunk|
-      samples = chunk.map do |d|
-        Sample.new(
-          performance_index: d[:t],
-          performance_id: performance_id,
-          accel_x: d[:x],
-          accel_y: d[:y],
-          accel_z: d[:z],
-          gyro_x: d[:rx],
-          gyro_y: d[:ry],
-          gyro_z: d[:rz],
-          )
-      end
-      Sample.import samples
+    data = SmarterCSV.process(path)
+    samples = data.map do |d|
+      Sample.new(
+        performance_index: d[:t],
+        performance_id: performance_id,
+        accel_x: d[:x],
+        accel_y: d[:y],
+        accel_z: d[:z],
+        gyro_x: d[:rx],
+        gyro_y: d[:ry],
+        gyro_z: d[:rz],
+        )
     end
+    Sample.import samples
   end
-  #
-  #
-  #   data = SmarterCSV.process(path)
-  #   samples = data.map do |d|
-  #     Sample.new(
-  #       performance_index: d[:t],
-  #       performance_id: performance_id,
-  #       accel_x: d[:x],
-  #       accel_y: d[:y],
-  #       accel_z: d[:z],
-  #       gyro_x: d[:rx],
-  #       gyro_y: d[:ry],
-  #       gyro_z: d[:rz],
-  #       )
-  #   end
-  #   Sample.import samples
-  # end
 
 
 end
